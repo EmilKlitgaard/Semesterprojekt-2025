a1bc596dd958248040c901bc1b4cce9128badd69
Mbank23 <Mbank23@student.sdu.dk>
2025-05-14 23:20:08 +0200

    moved methods to .cpp

--- GUI/GUI.h
+++ GUI/GUI.h
@@ -6,53 +6,27 @@
 class GUI {
 public:
     //set
-    void setDifficulty(int value){
-        if(!gameActive) {
-            if (value < 300) {
-                difficulty = 300;
-            }
-            if (value > 3000) {
-                difficulty = 3000;
-            }
-            difficulty = value;
-        }
+    void setDifficulty(int value);
 
-        /*
-        setoption name UCI_LimitStrength value true
-        setoption name UCI_Elo value 1200 
-        */
+    void setGameActive(bool value);
 
-    }
+    void setTurn(bool value);
 
-    void setGameActive(bool value) {
-        gameActive = value;
-    }
-
-    void setTurn(bool value) {
-        turn = value; // 1 = player, 0 = robot
-    }
-
-    void setConnection(bool value) {
-        connection = value; // 1 = connected, 0 = disconnected
-    }
+    void setConnection(bool value);
 
     // feed livefeed to GUI
-    void GUI::setVision(const cv::Mat& image) {
-        cvImage = image.clone();
-    }   
+    void setVision(const cv::Mat& image);
 
     //get
     int  getDifficulty() { return difficulty; }
     bool getGameActive() { return gameActive; }
     bool getTurn() { return turn; }
     bool getConnection() { return connection; }    
-    cv::Mat GUI::getVision() const { return cvImage.clone(); }
+    cv::Mat getVision() const;
 
 private:
-
-    cv::Mat cvImage; // contain image
-
     // param
+    cv::Mat cvImage;
     bool turn = true;
     bool connection = false;
     bool gameActive = false;
---
+++ GUI/ignore old header/GUI copy.h
@@ -0,0 +1,62 @@
+#pragma once
+#include<QApplication>
+#include <opencv2/opencv.hpp>
+
+
+class GUI {
+public:
+    //set
+    void setDifficulty(int value){
+        if(!gameActive) {
+            if (value < 300) {
+                difficulty = 300;
+            }
+            if (value > 3000) {
+                difficulty = 3000;
+            }
+            difficulty = value;
+        }
+
+        /*
+        setoption name UCI_LimitStrength value true
+        setoption name UCI_Elo value 1200 
+        */
+
+    }
+
+    void setGameActive(bool value) {
+        gameActive = value;
+    }
+
+    void setTurn(bool value) {
+        turn = value; // 1 = player, 0 = robot
+    }
+
+    void setConnection(bool value) {
+        connection = value; // 1 = connected, 0 = disconnected
+    }
+
+    // feed livefeed to GUI
+    void GUI::setVision(const cv::Mat& image) {
+        cvImage = image.clone();
+    }   
+
+    //get
+    int  getDifficulty() { return difficulty; }
+    bool getGameActive() { return gameActive; }
+    bool getTurn() { return turn; }
+    bool getConnection() { return connection; }    
+    cv::Mat GUI::getVision() const { return cvImage.clone(); }
+
+private:
+
+    cv::Mat cvImage; // contain image
+
+    // param
+    bool turn = true;
+    bool connection = false;
+    bool gameActive = false;
+    int difficulty = 300;
+};
+
+extern GUI gui;
